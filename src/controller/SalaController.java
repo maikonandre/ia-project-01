28dbef5373030adeb58a71c8ef5b999e8a6991d5
Arthur Vinicius Koloda <nwroleplay@gmail.com>
2021-07-14 08:53:25 -0300

    Criado algoritmo Hill climbing
    Criado o primeiro protótipo do que seria  o cálculo por Hill-climbing

--- src/controller/SalaController.java
+++ src/controller/SalaController.java
@@ -55,4 +55,129 @@ public class SalaController {
         
         return salas;
     }
+    
+    public SalaVO FindById(String id) {
+        
+        SalaVO[] salas = this.CarregarSalas();
+        SalaVO sala = new SalaVO();
+        
+        try{            
+            for (SalaVO s : salas) {
+                if (s.getId() == null ? id == null : s.getId().equals(id)) {
+                    sala = s;
+                    break;
+                }
+            }
+        }
+        catch (Exception ex) {
+            System.out.println("Erro: " + ex.getMessage());
+        }
+        
+        return sala;
+    }
+    
+    public int CalcularDiferenca(SalaVO sala1, SalaVO sala2){
+        int calcX = sala2.getX() - sala1.getX();
+        int calcY = sala2.getY() - sala1.getY();
+        
+        calcX = calcX < 0 ? (calcX * -1) : calcX;
+        calcY = calcY < 0 ? (calcY * -1) : calcY;
+        
+        return calcX + calcY;
+    }
+    
+    public String CalcularRota(SalaVO salaInicial, SalaVO salaFinal) {
+        
+        String[] caminho = new String[100];
+        
+        caminho[0] = salaInicial.getId();
+        
+        try{        
+            int loop = 1;
+
+            while(CalcularDiferenca(salaInicial, salaFinal) != 0){
+                caminho[loop] = salaInicial.getId();
+
+                String[] vizinhos = salaInicial.getVizinhos();
+                String[] provaveisCaminhos = new String[5];
+                
+                int loop2 = 0;
+
+                for(String s : vizinhos){
+                    boolean passou = false;
+
+                    for(String s1 : caminho){
+                        if(s1 != null){
+                            if(s1.equals(s)){
+                                passou = true;
+                            }
+                        }
+                        else{
+                            break;
+                        }
+                    }
+
+                    if(!passou){
+                        provaveisCaminhos[loop2] = s;
+                        
+                        loop2 += 1;
+                    }
+                }
+                
+                if(provaveisCaminhos.length == 0) {
+                        Exception ex = new Exception("Nenhum caminho possível foi encontrado!");
+                        throw ex;
+                    }
+                    else{
+                        String caminhoMenor = "";
+                        int caminhoMenorValor = 100;
+                        
+                        for(String s2 : provaveisCaminhos){
+                            
+                            if(s2 != null){
+                                SalaVO salaTeste = this.FindById(s2);
+                                int diferencaTeste = this.CalcularDiferenca(salaTeste, salaFinal);
+
+                                if(diferencaTeste < caminhoMenorValor){
+                                    caminhoMenorValor = diferencaTeste;
+                                    caminhoMenor = salaTeste.getId();
+                                }
+                            }
+                            else{
+                                break;
+                            }
+                        }
+                        
+                        caminho[loop] = caminhoMenor;
+                        
+                        salaInicial = this.FindById(caminhoMenor);
+                        
+                        loop += 1;
+                    }
+            }   
+        }     
+        catch (Exception ex){
+            
+        }
+        
+        String rota = "";
+        
+        if(caminho.length > 0){        
+            for(String s : caminho){
+                if(s != null){
+                    rota+= s + ", ";
+                }
+                else{
+                    break;
+                }
+            }
+
+            rota = rota.substring(0, rota.length() - 2);
+        }
+        else{
+            rota = "Não foi possível encontrar um caminho!";
+        }
+        
+        return rota;
+    }
 }
--- src/index.java
+++ src/index.java
@@ -14,5 +14,12 @@ public class index {
         //Início da aplicação
         SalaController controllerSala = new SalaController();
         salas = controllerSala.CarregarSalas();
+        
+        SalaVO salaInicial = controllerSala.FindById("T");
+        SalaVO salaFinal = controllerSala.FindById("C");
+        
+        String rota = controllerSala.CalcularRota(salaInicial, salaFinal);
+        
+        System.out.println("Rota percorrida de " + salaInicial.getId() + " a " + salaFinal.getId() + ": " + rota);
     }
 }
